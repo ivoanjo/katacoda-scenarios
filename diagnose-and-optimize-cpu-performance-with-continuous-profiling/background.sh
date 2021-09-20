21cd9ba1d6ac324294a24426c79e09d7e9d7c171
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-09-20 16:13:15 +0100

    WIP first step

--- diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
+++ diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
@@ -2,4 +2,8 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
+git clone https://github.com/DataDog/dd-continuous-profiler-example.git
+
+cd dd-continuous-profiler-example && echo "DD_API_KEY=$DD_API_KEY" > docker.env
+
 statusupdate "Bootstrap"
