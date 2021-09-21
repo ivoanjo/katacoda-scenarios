bd5fa934865c1108eb71e68f80a2fc4929530a96
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-09-21 09:21:02 +0100

    Get assets from git instead

--- diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
+++ diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
@@ -2,4 +2,10 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-statusupdate "Bootstrap"
+cd /root/lab
+# FIXME: Should be moved back to a datadog repo
+git clone https://github.com/ivoanjo/dd-continuous-profiler-example.git
+cd dd-continuous-profiler-example
+echo "DD_API_KEY=$DD_API_KEY" > docker.env
+
+statusupdate "Starting up..."
--- diagnose-and-optimize-cpu-performance-with-continuous-profiling/foreground.sh
+++ diagnose-and-optimize-cpu-performance-with-continuous-profiling/foreground.sh
@@ -2,18 +2,7 @@
 
 statuscheck "Bootstrap"
 
-echo "background"
-
-tree /root/dd-continuous-profiler-example
-
-cp -r /root/dd-continuous-profiler-example /root/lab/.
-
-cd dd-continuous-profiler-example
-echo "DD_API_KEY=$DD_API_KEY" > docker.env
-
-echo "foreground"
-
-cd /root/lab
+cd /root/lab/ && clear
 
 cd dd-continuous-profiler-example
 
