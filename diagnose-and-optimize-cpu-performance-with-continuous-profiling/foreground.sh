c5e5aaecc2500ba495cc049de4c670bfb3aca277
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-10-07 10:25:18 +0100

    WIP scenario setup

--- diagnose-and-optimize-cpu-performance-with-continuous-profiling/foreground.sh
+++ diagnose-and-optimize-cpu-performance-with-continuous-profiling/foreground.sh
@@ -2,9 +2,3 @@
 statuscheck "environment to be ready..."
 cd /root/lab/ && clear
 cd dd-continuous-profiler-example/java
-
-# Start up mongodb
-docker run -p 27017:27017 -v `pwd`/mongo-seed:/docker-entrypoint-initdb.d mongo:latest
-
-# Prewarm gradle
-./gradlew
