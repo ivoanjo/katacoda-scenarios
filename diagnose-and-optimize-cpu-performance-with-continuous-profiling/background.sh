47505383402c734bee75d901c929956263c772d0
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-10-19 15:36:42 +0100

    Make background.sh clean up existing state
    Hopefully this will give learners a clean environment, even if katacoda
    does not give them one.

--- diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
+++ diagnose-and-optimize-cpu-performance-with-continuous-profiling/background.sh
@@ -3,6 +3,17 @@
 curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 cd /root/lab
+
+# Remove any leftover state (some katacoda setups leave state between browser refreshes)
+# 1. stuff running in docker
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+# 2. movies-api repository
+rm -rf dd-continuous-profiler-dash2021/
+# 3. stop any leftover copies of movies-api-java
+pkill -f dd-continuous-profiler-dash2021
+# -----
+
 git clone https://github.com/DataDog/dd-continuous-profiler-dash2021.git
 
 cd dd-continuous-profiler-dash2021
